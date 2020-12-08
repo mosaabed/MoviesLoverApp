d8e8a0fe7815434e316b65dfc8ede207e91100bb
sana mahamid <sanaa_6-5.7@hotmail.com>
2020-12-08 12:40:33 +0200

    schemas done

---
+++ server/models/PostModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const PostSchema = new Schema({
+    text: String,
+    likes: Number,
+    userName: String,
+    comments: [{
+        userName: String,
+        text: String
+    }]
+})
+
+const Post = mongoose.model("PostModel", PostSchema)
+module.exports = Post
